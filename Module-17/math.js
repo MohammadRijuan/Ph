f28837da9cbdb5c955387113942c398e7ef079fe
Rijuan <mohammadrijuan2529@gmail.com>
2025-02-05 11:48:26 +0600

    math for programming

---
+++ Module-17/math.js
@@ -0,0 +1,12 @@
+// math for programming
+
+// 1. number : +ve ,-ve, 0
+// 2. integer : 2,88 , 15, 16 [odd, even]
+// 3. float : 2.2 , 1.3 , 4.7
+// 4. operation : +, -, * ,/
+// 5. more operation : %, [ ]
+// 6. percentage : 50% ,5%, 2%
+// 7. sum :14, 18 , 16
+// 8. Avg : 14 ,22 ,20
+// 9. max/ min : 14 18 16
+// 10. Area : Triangle
