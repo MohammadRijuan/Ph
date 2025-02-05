e2bf6bc056f8d283103ef78e78c38f35e764efab
Rijuan <mohammadrijuan2529@gmail.com>
2025-02-05 11:58:55 +0600

    variable declaration

---
+++ Module-17/variable.js
@@ -0,0 +1,22 @@
+// variable ==> vary + able
+// 
+// 5 things to declare a varibale
+
+
+var weight = 50;
+var price = 150;
+var year = 2000;
+var money = 50000;
+
+money=2000;
+
+// will show an error
+money=$2000;
+// output undefined bcoz i didnot declare age
+var age; 
+
+console.log(weight)
+console.log(price)
+console.log(year)
+console.log(money)
+console.log(age)
